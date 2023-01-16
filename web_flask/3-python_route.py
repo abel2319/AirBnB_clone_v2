4d4a4471241d837bb002611d3d695f4eb5291ed4
abel2319 <abelbakary@gmail.com>
2023-01-16 03:05:03 +0100

    task3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application
+"""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text):
+    return 'C %s' % text.replace('_', ' ')
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def display_python(text="is cool"):
+    return 'Python %s' % text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
